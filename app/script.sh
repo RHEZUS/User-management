15bcbac571de44b4dbe2d48187f89e0ba4d35f3b
RHEZUS <ludivinseunda2@gmail.com>
2023-09-10 12:13:40 -0700

    create create app, update, install and script files with functions

---
+++ app/script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# main - starting point
+# 
+#
+function main ()
+{
+}
+
+function createUser()
+{
+
+}
+
+function changePassword()
+{
+
+}
+
+function deleteUser()
+{
+
+}
+
+function listUsers ()
+{
+
+}
